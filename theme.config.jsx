39482a356874fd5031f5ea59a879180ce6af82c0
Vasco Ramalho <vascopcmr@gmail.com>
2024-10-18 15:02:21 +0100

    changed link of edit

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
   logo: <span>Altar Blog</span>,
   project: {
-    link: "https://github.com/vmagellan/altar-blog/",
+    link: "https://github.com/vmagellan/altar-blog/blob/main/",
   },
   // ... other theme options
 };
